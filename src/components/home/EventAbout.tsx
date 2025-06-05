36669b32c306346b8f21a9a2b5b2acb7b3a5a712
Sasmita Mahanta <sasmitam@netsqure.com>
2025-06-05 14:59:15 +0530

    small changes

--- src/components/home/EventAbout.tsx
+++ src/components/home/EventAbout.tsx
@@ -37,46 +37,47 @@ const EventAbout = () => {
   }, []);
   return (
     <div className="main-container flex items-center justify-center py-16">
-      <div className="flex lg:flex-row flex-col justify-center items-center gap-8 w-full">
-        {/* Left Section */}
+      <div className="flex flex-col justify-center items-center gap-8 w-full">
         <div className="w-full" ref={headRef}>
           <p className="text-5xl">About our</p>
           <p className="text-yellow-600 text-7xl">Event</p>
-          <div className="mt-6">
+        </div>
+        <div className="flex lg:flex-row flex-col w-full">
+          <div className="mt-10 w-full">
             <img
               src="/image_1.png"
               alt="image"
-              className="w-full h-auto rounded-md"
+              className="w-full lg:w-[70rem] h-auto rounded-md"
             />
           </div>
-        </div>
 
-        {/* Right Section */}
-        <div
-          className="flex flex-col gap-5 justify-start items-start w-full"
-          ref={containerRef}
-        >
-          <p>
-            Saudi Arabia’s Real Estate Market size is estimated at USD 69.51
-            billion in 2024, and is expected to reach USD 101.62 billion by
-            2029. Saudi Arabia has identified housing, tourism and commercial as
-            some of its key projects under Vision 2030. As the demand continues
-            to surpass supply in these sectors, owners, developers, operators,
-            contractors, architects, designers and consultants are working
-            wonders to create projects of the future, projects that redefine
-            human limitations, projects that push the boundaries of
-            possibilities. What they need are, right partners and products to
-            count on.
-          </p>
-          <p>
-            Real Estate Development Summit provides a gateway to these
-            possibilities. By efficiently putting your time to use for valuable
-            knowledge exchange, guidance of the thought leaders and most
-            importantly connecting you, to the right people with right
-            requirement at the right time. Breaking the confines of the
-            conventional, embark on this journey to explore the world of
-            opportunities as we are Connecting Beyond Boundaries!
-          </p>
+          {/* Right Section */}
+          <div
+            className="flex flex-col gap-5 justify-start items-start w-2/3 lg:pl-[5rem]"
+            ref={containerRef}
+          >
+            <p>
+              Saudi Arabia’s Real Estate Market size is estimated at USD 69.51
+              billion in 2024, and is expected to reach USD 101.62 billion by
+              2029. Saudi Arabia has identified housing, tourism and commercial
+              as some of its key projects under Vision 2030. As the demand
+              continues to surpass supply in these sectors, owners, developers,
+              operators, contractors, architects, designers and consultants are
+              working wonders to create projects of the future, projects that
+              redefine human limitations, projects that push the boundaries of
+              possibilities. What they need are, right partners and products to
+              count on.
+            </p>
+            <p>
+              Real Estate Development Summit provides a gateway to these
+              possibilities. By efficiently putting your time to use for
+              valuable knowledge exchange, guidance of the thought leaders and
+              most importantly connecting you, to the right people with right
+              requirement at the right time. Breaking the confines of the
+              conventional, embark on this journey to explore the world of
+              opportunities as we are Connecting Beyond Boundaries!
+            </p>
+          </div>
         </div>
       </div>
     </div>
