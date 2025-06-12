7e2e7fc82c2919828c4f0f81783ac85215c606f4
Bikash Sasmal <bikashs@netsqure.com>
2025-06-12 20:48:04 +0530

    Updated the publicLayout for smoothness in whole website

--- src/layouts/PublicLayout.tsx
+++ src/layouts/PublicLayout.tsx
@@ -1,16 +1,25 @@
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
 import { useEffect, useRef } from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
 import Header from "./Header";
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 const PublicLayout = () => {
   const headerInnerRef = useRef(null);
 
   useEffect(() => {
+    const smoother = ScrollSmoother.create({
+      smooth: 2,
+      effects: true,
+      smoothTouch: 0.1,
+      wrapper: "#smooth-wrapper",
+      content: "#smooth-content",
+    });
+
     gsap.set(headerInnerRef.current, {
       width: "70%",
       borderBottom: "none",
@@ -28,30 +37,37 @@ const PublicLayout = () => {
         scrub: false,
       },
     });
+
+    return () => {
+      smoother.kill();
+      ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+    };
   }, []);
+
   return (
-    <div className="flex flex-col">
-      {/* Header */}
+    <div id="smooth-wrapper">
+      <div id="smooth-content" className="flex flex-col">
+        {/* Header */}
+        <header className="bg-black text-white flex justify-center items-center fixed top-0 w-full z-50">
+          <div
+            ref={headerInnerRef}
+            className="transition-all duration-1000 ease-in-out"
+            style={{ borderBottom: "none" }}
+          >
+            <Header />
+          </div>
+        </header>
 
-      <header className="bg-black text-white flex justify-center items-center fixed top-0 w-full z-50">
-        <div
-          ref={headerInnerRef}
-          className="transition-all duration-1000 ease-in-out"
-          style={{ borderBottom: "none" }}
-        >
-          <Header />
-        </div>
-      </header>
+        {/* Main content */}
+        <main className="flex-1">
+          <Outlet />
+        </main>
 
-      {/* Main content */}
-      <main className="flex-1  overflow-y-auto">
-        <Outlet />
-      </main>
-
-      {/* Footer */}
-      <footer className=" text-white">
-        <Footer />
-      </footer>
+        {/* Footer */}
+        <footer className="text-white">
+          <Footer />
+        </footer>
+      </div>
     </div>
   );
 };
