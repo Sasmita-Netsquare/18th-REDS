9947c339260433b4f14d85a899cf058787ab0bb3
Bikash Sasmal <bikashs@netsqure.com>
2025-06-12 20:53:49 +0530

    updated the section title

--- src/components/home/SectionTitle.tsx
+++ src/components/home/SectionTitle.tsx
@@ -8,9 +8,9 @@ interface SectionTitleProps {
 const SectionTitle = forwardRef<HTMLDivElement, SectionTitleProps>(
   ({ title, subtitle }, ref) => {
     return (
-      <div className="w-full lg:px-[0.03rem] md:px-[0.13rem]" ref={ref}>
-        <p className="lg:text-5xl md:text-5xl text-3xl">{title}</p>
-        <p className="text-yellow-600 lg:text-8xl md:text-6xl text-5xl">
+       <div className="w-full lg:px-[0.03rem] md:px-[0.13rem]" ref={ref}>
+        <p className="lg:text-5xl md:text-5xl text-3xl ">{title}</p>
+        <p className="text-yellow-600 lg:text-8xl md:text-6xl text-5xl transfor translate-y-full">
           {subtitle}
         </p>
       </div>
